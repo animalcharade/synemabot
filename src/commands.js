db4eff6b47d4f38c26a5e71f6c6bbb2f58cd511e
Michael Aranda <michaelaranda@synemastudios.com>
2019-06-14 01:01:14 -0600

    Adds Discord command

--- src/commands.js
+++ src/commands.js
@@ -3,6 +3,7 @@ const functions = require('./functions');
 const MINUTE = 60000;
 
 const commands = {
+  discord: functions.globalCooldown(MINUTE, functions.say('Join the Arandanauts Discord! https://discord.gg/SPHTCyN')),
   falgsc: functions.globalCooldown(MINUTE, functions.falgsc()),
   kingofpeen: functions.globalCooldown(MINUTE, functions.say('All hail The King Of Peen! Let us rejoice and remember His great coronation! https://www.twitch.tv/michaelaranda/clip/JazzyFantasticCattleShazBotstix')),
   lol: functions.globalCooldown(MINUTE, functions.say('lol')),
