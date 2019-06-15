1a0a78c37f02d8a1eace7dfd7fc58dd1c0d0f3bd
Michael Aranda <michaelaranda@synemastudios.com>
2019-06-15 02:09:35 -0600

    Changed FALGSC command per-user cooldown

--- src/commands.js
+++ src/commands.js
@@ -5,7 +5,7 @@ const MINUTE = 60000;
 const commands = {
   discord: functions.globalCooldown(MINUTE, functions.say('Join the Arandanauts Discord! https://discord.gg/SPHTCyN')),
   donate: functions.globalCooldown(MINUTE, functions.say('https://streamlabs.com/michaelaranda')),
-  falgsc: functions.globalCooldown(MINUTE, functions.falgsc()),
+  falgsc: functions.userCooldown(MINUTE * 10, functions.falgsc()),
   kingofpeen: functions.globalCooldown(MINUTE, functions.say('All hail The King Of Peen! Let us rejoice and remember His great coronation! https://www.twitch.tv/michaelaranda/clip/JazzyFantasticCattleShazBotstix')),
   lol: functions.globalCooldown(MINUTE, functions.say('lol')),
   lurk(client, command) {
