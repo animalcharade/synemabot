48919248ebcf7a9ba91fdd086347e57cb1aa3e52
Michael Aranda <michaelaranda@synemastudios.com>
2019-08-15 02:57:01 -0600

    Makes Twitch chat/whisper commands asynchronous
    Allows the bot to wait for Twitch if it's being slow to respond

--- src/functions/mcr.js
+++ src/functions/mcr.js
@@ -4,12 +4,12 @@ module.exports = function mcr() {
   const lyrics = ['WHEN I WAS...', 'A YOUNG BOY...', 'MY FATHER...', 'TOOK ME INTO THE CITY...', 'TO SEE A MARCHING BAND...'];
 
 
-  return function mcrHandler(client, command) {
+  return async function mcrHandler(client, command) {
     if (command.username.toLowerCase() === 'tsimants') {
-      client.chat.say(command.channel, 'https://www.youtube.com/watch?v=0f03ZKjtuKM');
+      await client.chat.say(command.channel, 'https://www.youtube.com/watch?v=0f03ZKjtuKM');
       songPosition = 0;
     } else {
-      client.chat.say(command.channel, lyrics[songPosition]);
+      await client.chat.say(command.channel, lyrics[songPosition]);
       if (songPosition < lyrics.length - 1) {
         songPosition += 1;
       } else {
