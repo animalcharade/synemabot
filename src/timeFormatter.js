ba0615b59b23af972254bc0bbb0ef6c22105da63
Michael Aranda <michaelaranda@synemastudios.com>
2019-08-15 17:52:56 -0600

    Adds detailed comments to timeFormatter

--- src/timeFormatter.js
+++ src/timeFormatter.js
@@ -1,15 +1,25 @@
 module.exports = function timeFormatter(timeInput) {
   // Convert from milliseconds to seconds
   let time = Math.floor(timeInput / 1000);
+
+  // Parse out the number of leftover seconds for the final display
   const seconds = time % 60;
+  // Remove those seconds from our time variable
   time = Math.floor(time / 60);
+  // Parse out the number of leftover minutes for the final display
   const minutes = time % 60;
+  // Remove those minutes from the final display
   time = Math.floor(time / 60);
+  // The remaining time is our number of hours
   const hours = time;
+
+  // Build the output string from the back, starting with the end
   let output = seconds + ' seconds';
+  // If we have minutes to display, add them to the front of our string
   if (minutes > 0) {
     output = minutes + ' minutes, ' + output;
   }
+  // If we have any hours to display, add them to the front of our string
   if (hours > 0) {
     output = hours + ' hours, ' + output;
   }
