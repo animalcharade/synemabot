87ddef322ca4b61615719c782ab83035a1e8ba78
Michael Aranda <michaelaranda@synemastudios.com>
2019-08-17 08:12:37 -0600

    Allows MCR command to check falgsc automatedness
    Refactors falgsc.js to allow external functions to check whether its
    automatedness variable is >= 100.
    
    Changes reference to falgsc.js in commands.js to compensate for the
    above change.
    
    Adds functionality to mcr.js to check falgc.js's automatedness when a
    particular uses the command.

--- src/functions/falgsc.js
+++ src/functions/falgsc.js
@@ -1,23 +1,31 @@
-module.exports = function falgsc() {
-  let automatedness = 0;
+let automatedness = 0;
 
-  function decay() {
-    if (automatedness > 0) {
-      automatedness -= 1;
+module.exports = {
+  falgsc() {
+    function decay() {
+      if (automatedness > 0) {
+        automatedness -= 1;
+      }
     }
-  }
 
-  setInterval(decay, 1000 * 60 * 2);
-  return async function falgscHandler(client, command) {
-    if (automatedness < 100) {
-      automatedness += 10;
+    setInterval(decay, 1000 * 60 * 2);
+    return async function falgscHandler(client, command) {
+      if (automatedness < 100) {
+        automatedness += 10;
+      }
+      let output;
+      if (automatedness < 100) {
+        output = '/me The luxury gay space communism is ' + automatedness + '% automated.';
+      } else {
+        output = '/me The luxury gay space communism is FULLY AUTOMATED.';
+      }
+      await client.chat.say(command.channel, output);
+    };
+  },
+  isFullyAutomated() {
+    if (automatedness >= 100) {
+      return true;
     }
-    let output;
-    if (automatedness < 100) {
-      output = '/me The luxury gay space communism is ' + automatedness + '% automated.';
-    } else {
-      output = '/me The luxury gay space communism is FULLY AUTOMATED.';
-    }
-    await client.chat.say(command.channel, output);
-  };
+    return false;
+  },
 };
