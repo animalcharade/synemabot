77af732e879740a68cd52bce5d70760549ba0428
Michael Aranda <michaelaranda@synemastudios.com>
2019-08-17 09:38:04 -0600

    Refactors falgsc.js to prettify the code

--- src/functions/falgsc.js
+++ src/functions/falgsc.js
@@ -1,31 +1,38 @@
 let automatedness = 0;
 
-module.exports = {
-  falgsc() {
-    function decay() {
-      if (automatedness > 0) {
-        automatedness -= 1;
-      }
-    }
+// If automatedness is above zero, move the value toward zero when this function is called
+function decay() {
+  if (automatedness > 0) {
+    automatedness -= 1;
+  }
+}
+// Every two minutes, call decay()
+setInterval(decay, 1000 * 60 * 2);
 
-    setInterval(decay, 1000 * 60 * 2);
-    return async function falgscHandler(client, command) {
-      if (automatedness < 100) {
-        automatedness += 10;
-      }
-      let output;
-      if (automatedness < 100) {
-        output = '/me The luxury gay space communism is ' + automatedness + '% automated.';
-      } else {
-        output = '/me The luxury gay space communism is FULLY AUTOMATED.';
-      }
-      await client.chat.say(command.channel, output);
-    };
-  },
-  isFullyAutomated() {
-    if (automatedness >= 100) {
-      return true;
+// Is the luxury gay space communism fully automated?
+function isFullyAutomated() {
+  if (automatedness >= 100) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  // When the falgsc command is used...
+  async handler(client, command) {
+    // If we've not yet made it too 100 or above, add 10
+    if (automatedness < 100) {
+      automatedness += 10;
     }
-    return false;
+    // Let's let the people know how automated the luxury gay space communism is!
+    let output;
+    if (isFullyAutomated() === false) {
+      output = '/me The luxury gay space communism is ' + automatedness + '% automated.';
+    } else {
+      output = '/me The luxury gay space communism is FULLY AUTOMATED.';
+    }
+    await client.chat.say(command.channel, output);
   },
+  // Make the isFullyAutomated() function available to other functions
+  isFullyAutomated,
 };
