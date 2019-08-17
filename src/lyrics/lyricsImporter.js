ff849a08c19a9af2d08d8e27384f1445b3730375
Michael Aranda <47492532+animalcharade@users.noreply.github.com>
2019-08-17 13:00:06 -0600

    Adds a lyrics file importer
    And modifies mcr.js to use it

---
+++ src/lyrics/lyricsImporter.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+module.exports = function lyricsImporter(path) {
+  let file;
+  // Try to import the contents of the specified file
+  try {
+    file = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    console.log('LyricsImporter is sad! No such file: ' + path);
+    throw err;
+  }
+  // Split the file into an array by newlines
+  let lyrics = file.split('\n');
+  // Remove empty items
+  lyrics = lyrics.filter(x => x);
+  // Make everything uppercase
+  lyrics = lyrics.map(x => x.toUpperCase());
+  // Strip unnecessary puntuation
+  lyrics = lyrics.map(x => x.replace(/[?".,!()]/g, ''));
+  // Add ... to each item
+  lyrics = lyrics.map(x => x + '...');
+  // Return the resulting array
+  return lyrics;
+};
