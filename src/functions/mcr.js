66c42468c25d099f45304e13faa512c2e6cfe498
Michael Aranda <47492532+animalcharade@users.noreply.github.com>
2019-08-17 09:50:24 -0600

    Allows MCR command to check falgsc automatedness (#2)
    Refactors falgsc.js to allow external functions to check whether its
    automatedness variable is >= 100. Also prettifies code and adds comments to improve readibility.
    
    Changes reference to falgsc.js in commands.js to compensate for the
    above change.
    
    Adds functionality to mcr.js to check falgc.js's automatedness when a
    particular uses the command.

--- src/functions/mcr.js
+++ src/functions/mcr.js
@@ -1,11 +1,13 @@
+const falgsc = require('./falgsc');
+
 module.exports = function mcr() {
   let songPosition = 0;
 
   const lyrics = ['WHEN I WAS...', 'A YOUNG BOY...', 'MY FATHER...', 'TOOK ME INTO THE CITY...', 'TO SEE A MARCHING BAND...'];
 
-
   return async function mcrHandler(client, command) {
-    if (command.username.toLowerCase() === 'tsimants') {
+    // If Tsimants uses the command, but the luxury gay space communism isn't fully automated, send a LASIK link instead!
+    if (command.username.toLowerCase() === 'tsimants' && falgsc.isFullyAutomated() === false) {
       await client.chat.say(command.channel, 'https://www.youtube.com/watch?v=0f03ZKjtuKM');
       songPosition = 0;
     } else {
