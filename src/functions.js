ad31fbbc6f54e95e4fa33490a71034b3b8a0a1e7
Michael Aranda <michaelaranda@synemastudios.com>
2019-08-21 06:02:25 -0600

    Refactors functions.js to ignore test files

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 // Copyright (C) 2019 Synema Studios
 //
 // This file is part of SynemaBot.
@@ -15,7 +17,24 @@
 // You should have received a copy of the GNU General Public License
 // along with SynemaBot.  If not, see <http://www.gnu.org/licenses/>.
 
+const fs = require('fs');
 const path = require('path');
-const autoload = require('auto-load');
 
-module.exports = autoload(path.join(__dirname, 'functions'));
+const functions = {};
+
+const dirPath = path.join(__dirname, 'functions');
+
+fs.readdirSync(dirPath).forEach((file) => {
+  // Is this file a test file?
+  if (file.includes('.test.js')) {
+    return;
+  }
+
+  // Get the name of the file, sans extension
+  const command = file.substr(0, file.length - 3);
+
+  // Add the name of the file to our commands object
+  functions[command] = require(path.join(dirPath, command));
+});
+
+module.exports = functions;
--- src/functions/say.test.js
+++ src/functions/say.test.js
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with SynemaBot.  If not, see <http://www.gnu.org/licenses/>.
 
-const say = require('../functions/say');
+const say = require('./say');
 
 describe('say()', () => {
   test('returns a function', () => {
